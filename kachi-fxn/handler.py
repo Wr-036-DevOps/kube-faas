c1408822c629026aabd576a383d607eade3f97e2
Ak <akachukwumba@gmail.com>
2022-12-30 16:13:14 +0100

    Added upload to s3 feature

--- kachi-fxn/handler.py
+++ kachi-fxn/handler.py
@@ -1,5 +1,7 @@
 from bing_image_downloader import downloader
 import json
+import boto3
+import os
 
 
 def handle(event):
@@ -15,7 +17,30 @@ def handle(event):
     print(f"we are downloading {number} pictures of {animal}")
     downloader.download(f"{animal}", limit=number, output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60)
 
+    bucket_name = "wr-36-animal-images"
+    path = f"./dataset/{animal}/"
+    client = boto3.client("s3")
+
+
+    # Uploading downloaded images to s3
+    for i, image in enumerate(os.listdir(path)):
+        # Create the new filename using the animal name and the loop index
+        new_filename = f"{animal}-{i + 1}.jpg"
+
+        # Use the os.rename() function to rename the file
+        os.rename(os.path.join(path, image), os.path.join(path, new_filename))
+        key = new_filename
+
+        # Creating file name with full path for open()
+        file = str(path) + str(new_filename)
+        with open(file, "rb") as f:
+            client.put_object(
+                Bucket=bucket_name,
+                Body=f,
+                Key=key
+            )
+
     return {
         'statusCode': 200,
-        'body': f"download succeeded: download of {number} images of {animal} complete"
+        'body': f"upload succeeded: upload of {number} images of {animal} complete"
     }
